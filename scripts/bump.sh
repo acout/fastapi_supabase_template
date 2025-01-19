9500f418bd24d8fa70abbcf5590a7cb2202f8646
Atticuszz <1831768457@qq.com>
2025-01-19 17:28:25 +0800

    chore: update email

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # update CHANGELOG.md use GITHUB_REPO ENV as github token
-uvx git-cliff -o -v --github-repo "atticuszz/python-uv"
+uvx git-cliff -o -v --github-repo "atticuszeller/python-uv"
 # bump version and commit with tags
 # uvx bump-my-version bump patch
 # push remote
