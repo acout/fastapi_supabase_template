766229193d733bcf7a950ebad679a587a99fb0be
Atticuszz <1831768457@qq.com>
2025-01-30 17:29:34 +0800

    chore: update bump.sh

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # update CHANGELOG.md use GITHUB_REPO ENV as github token
-uvx git-cliff -o -v --github-repo "atticuszeller/python-uv"
+uvx git-cliff -o -v --github-repo "atticuszeller/fastapi_supabase_template"
 # bump version and commit with tags
-# uvx bump-my-version bump patch
+uvx bump-my-version bump patch
 # push remote
-# git push origin main --tags
+git push origin main --tags
