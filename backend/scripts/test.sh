6ddcfddf71a3d747dc880f94b4a2940e81758e15
Atticuszz <1831768457@qq.com>
2025-01-30 12:54:00 +0800

    chore: remove asyncio debug in pytest

--- backend/scripts/test.sh
+++ backend/scripts/test.sh
@@ -3,10 +3,6 @@
 set -e
 set -x
 
-# Enable asyncio debug mode
-# Ref: https://github.com/Kludex/fastapi-tips?tab=readme-ov-file#7-enable-asyncio-debug-mode
-export PYTHONASYNCIODEBUG=1
-
 coverage run --source=app -m pytest
 coverage report --show-missing
 coverage html --title "${@-coverage}"
