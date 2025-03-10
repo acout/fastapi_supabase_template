6c76878d0cb684000899801cf9fef9ca3ca37e77
Anthony Coutant <anthony.coutant@gmail.com>
2025-03-10 18:25:09 +0100

    Mise à jour du module models/__init__.py pour inclure les nouveaux modèles

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,13 +2,21 @@ from sqlmodel import SQLModel
 from .item import Item
 from .user import User
 from .profile import Profile, ProfilePicturesBucket
+from .file import FileMetadata
+from .storage import ProfilePictures, ItemDocuments
 
 # Pour Alembic
 Base = SQLModel
 
-__all__ = ["User", "Item", "Base", "Profile", "ProfilePicturesBucket"]
+__all__ = [
+    "User", "Item", "Base", "Profile", 
+    "ProfilePicturesBucket", "FileMetadata",
+    "ProfilePictures", "ItemDocuments"
+]
 
 STORAGE_BUCKETS = [
     ProfilePicturesBucket,
+    ProfilePictures,
+    ItemDocuments,
     # Ajoutez d'autres buckets ici...
 ]
