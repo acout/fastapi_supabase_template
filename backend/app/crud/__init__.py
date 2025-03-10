acfe5dec03b10f39ae775bd5d0188b8f630e3f83
Anthony Coutant <anthony.coutant@gmail.com>
2025-03-10 18:26:22 +0100

    Mise à jour du module crud/__init__.py pour inclure le nouveau modèle CRUD

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -1,7 +1,8 @@
 from .crud_item import item
+from .file import file_metadata
 
 # For a new basic set of CRUD operations you could just do
-__all__ = ["item"]
+__all__ = ["item", "file_metadata"]
 # from .base import CRUDBase
 # from app.models.item import Item
 # from app.schemas.item import ItemCreate, ItemUpdate
