dcd4c607efe5903d5075a9b2b695421fbce2c4e7
Anthony Coutant <anthony.coutant@gmail.com>
2025-03-10 18:28:23 +0100

    Compléter les dépendances pour injecter le service de stockage

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -2,12 +2,21 @@ from typing import Annotated
 
 from fastapi import Depends
 from sqlmodel import Session
+from supabase._async.client import AsyncClient
 
-from app.core.auth import get_current_user
+from app.core.auth import SuperClient, get_current_user
 from app.core.db import get_db
 from app.schemas.auth import UserIn
+from app.services import get_storage_service
+from app.services.storage import StorageService
 
 CurrentUser = Annotated[UserIn, Depends(get_current_user)]
-
-
 SessionDep = Annotated[Session, Depends(get_db)]
+
+
+async def get_storage_service_dep(supabase_client: SuperClient) -> StorageService:
+    """Dépendance pour injecter le service de stockage"""
+    return await get_storage_service(supabase_client)
+
+
+StorageServiceDep = Annotated[StorageService, Depends(get_storage_service_dep)]
